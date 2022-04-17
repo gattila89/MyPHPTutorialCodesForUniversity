db668e7289d4b86db9a0a55cea79d81dffe46723
gattila89 <gattila89@gmail.com>
2022-04-17 17:34:28 +0200

    User delete function

--- usersList.php
+++ usersList.php
@@ -1,33 +1,79 @@
 <?php
     $conn = new mysqli("localhost", "root", "", "iwyrwv_testdb");
-    
-    $sql = "SELECT * FROM Vevok";
-    $result = $conn->query($sql);
-
-    if($result->num_rows > 0)
+    $sql_query="SELECT * FROM Vevok";
+    $result_set=$conn->query($sql_query);
+    if(isset($_GET['delete_id']))
     {
-        echo "<table><tr><th>Azonosito</th><th>Vevo neve</th><th>Email</th><th>Telefon</th><th>Cim</th><th colspan='2'>Muveletek</th></tr>";
-        while($row = $result->fetch_assoc())
-        {
-            echo "<tr>
-                    <td>".$row["VevoID"]."</td>
-                    <td>".$row["nev"]."</td>
-                    <td>".$row["email"]."</td>
-                    <td>".$row["telefon"]."</td>
-                    <td>".$row["cim"]."</td>
-                    <td><a href='usersEdit.php'>Szerkeszt</a></td>
-                    <td><a href='usersDelete.php'>Töröl</a></td>
-                </tr>";
-        }
-        echo "</table>";
+        $sql_query="DELETE FROM Vevok WHERE VevoID=".$_GET['delete_id'];
+        $conn->query($sql_query);
+        header("Location: usersList.php");
     }
-    else {
-        echo "Nem talalhato Vevo az adatbazisban";
-    }
-
-    echo '<a href="usersAdd.php">Uj vevo hozzaadasa</a>';
-
-    $conn->close();
-    echo '<br>';
-    echo '<a href="index.html">Vissza</a>';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+    <script type="text/javascript">
+    
+    function delete_id(id)
+    {
+        if(confirm('Biztosan törölni akarod a vevöt?'))
+        {
+            window.location.href='usersList.php?delete_id='+id;
+        }
+    }
+
+    </script>
+
+</head>
+<body>
+    <table>
+    <tr>
+        <th>Azonosito</th>
+        <th>Nev</th>
+        <th>Email</th>
+        <th>Telefon</th>
+        <th>Cim</th>
+        <th colspan="2">Müveletek</th>
+    </tr>
+
+    <?php
+    if(mysqli_num_rows($result_set)>0)
+    {
+        while($row=mysqli_fetch_row($result_set))
+        {
+            ?>
+
+            <tr>
+                <td><?php echo $row[0]; ?></td>
+                <td><?php echo $row[1]; ?></td>
+                <td><?php echo $row[2]; ?></td>
+                <td><?php echo $row[3]; ?></td>
+                <td><?php echo $row[4]; ?></td>
+                <td><a href="usersEdit.php?id=<?php echo $row[0];?>">Szerkeszt</a></td>
+                <td><a href="javascript:delete_id(<?php echo $row[0]; ?>)">Töröl</a></td>
+            </tr>
+
+            <?php
+        }
+    }
+    else
+    {
+        ?>
+
+        <tr>
+        <th colspan="4">Nem talalhato Vevö az adatbazisban</th>
+        </tr>
+
+        <?php
+    }
+        ?>
+    </table>
+
+    <br>
+    <a href="usersAdd.php">Uj Vevo hozzaadasa</a>
+
+</body>
+</html>
