b486d7d47f0391dd6a1353062a00c5ed81cb4732
gattila89 <gattila89@gmail.com>
2022-04-19 20:45:24 +0200

    users List bootstrap table

--- usersList.php
+++ usersList.php
@@ -38,7 +38,7 @@
 
 </head>
 <body>
-    <table>
+    <table class="table">
     <tr>
         <th>Azonosito</th>
         <th>Nev</th>
@@ -82,7 +82,7 @@
     </table>
 
     <br>
-    <a href="usersAdd.php">Uj Vevo hozzaadasa</a>
+    <a href="usersAdd.php" class="btn">Uj Vevo hozzaadasa</a>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
